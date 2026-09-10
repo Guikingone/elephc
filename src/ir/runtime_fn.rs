9c2edb8e36c42f09e986d68791e1f0b68b02ec08
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2026-09-10 02:15:39 +0200

    fix(arrays): support boxed membership and callable comparisons

--- src/ir/runtime_fn.rs
+++ src/ir/runtime_fn.rs
@@ -1580,6 +1580,7 @@ impl RuntimeFnId {
             self,
             RuntimeFnId::Abs
                 | RuntimeFnId::Gettype
+                | RuntimeFnId::InArray
                 | RuntimeFnId::Trim
         )
     }
@@ -1602,7 +1603,7 @@ impl RuntimeFnId {
                         | PhpType::Void
                 )
             }),
-            RuntimeFnId::Gettype => true,
+            RuntimeFnId::Gettype | RuntimeFnId::InArray => true,
             RuntimeFnId::Trim => source.is_none_or(|ty| matches!(ty, PhpType::Str)),
             _ => false,
         }
